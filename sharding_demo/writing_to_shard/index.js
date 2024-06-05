8b7d435b900d826909ae831a01239791d967fadd
rohan843 <rohan.sharma2934@gmail.com>
2024-06-05 18:38:30 +0530

    doc: update notes

--- sharding_demo/writing_to_shard/index.js
+++ sharding_demo/writing_to_shard/index.js
@@ -11,19 +11,27 @@ const clients = {
   }),
   5433: new Client({
     host: "172.17.0.3",
-    port: "5432",
+    port: "5433",
     user: "postgres",
     password: "password",
     database: "postgres",
   }),
   5434: new Client({
     host: "172.17.0.4",
-    port: "5432",
+    port: "5434",
     user: "postgres",
     password: "password",
     database: "postgres",
   }),
 };
+
+async function connect() {
+    clients[5432].connect();
+    clients[5433].connect();
+    clients[5434].connect();
+}
+
+connect();
 
 app.get("/", (req, res) => {});
 
